d20a0de2d466e61e4efffb57aac5b1830b924379
umang-orpadia <orpadiau@gmail.com>
2024-01-01 23:46:14 +0530

    Improved readbility and removed unwanted repetation

--- Replica_SMS.py
+++ Replica_SMS.py
@@ -4,6 +4,18 @@ import mysql.connector
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~First Window~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def labelthisas(mastername , displaytext , r,c):
+    labelthisas = Label(mastername , text=displaytext)
+    labelthisas.grid(row=r , column=c ,padx=20 , pady=20)
+
+def entryfunction(mastername , r, c):
+    entry = Entry(mastername)
+    entry.grid(row=r , column=c)
+    return entry
+
+def btn(mastername , btntext,givecommand,r,c):
+    Button(mastername , text=btntext , command=givecommand ).grid(row=r , column=c , padx=20 , pady=20)
+
 def login_window_function():
 
 
@@ -12,12 +24,11 @@ def login_window_function():
     login_window.geometry("800x600") # Defines the dimention of the window
     login_window.configure(background="blue") #Decides the color of the page
 
-    user_label = Label(login_window,text="username").grid(row=0 , column=0 ,padx=20 ,pady=20)
-    password_label = Label(login_window,text="password").grid(row=1 , column=0 ,padx=20 ,pady=20)
-    username_entry = Entry(login_window)
-    username_entry.grid(row=0 , column=1 )
-    password_entry = Entry(login_window)
-    password_entry.grid(row=1 , column=1 )
+
+    labelthisas(login_window ,'Username',0,0)
+    labelthisas(login_window,'Password',1,0)
+    username_entry =entryfunction(login_window,0,1)
+    password_entry = entryfunction(login_window,1,1)
 
     conn = mysql.connector.connect(host = "localhost" , user = "root" , password = "")
     mycursor = conn.cursor()
@@ -39,21 +50,16 @@ def login_window_function():
                     register_user_window.configure(background='purple')
 
 
-                    student_name_label = Label(register_user_window,text='Name').grid(row = 0 , column = 0 , padx = 20 , pady = 20)
-                    student_name_entry = Entry(register_user_window)
-                    student_name_entry.grid(row = 0 , column = 1 , padx = 20 , pady = 20)
-                    student_roll_label = Label(register_user_window,text='Roll no : ').grid(row = 1 , column = 0 , padx = 20 , pady = 20)
-                    student_roll_entry = Entry(register_user_window)
-                    student_roll_entry.grid(row = 1 , column = 1 , padx = 20 , pady = 20)
-                    student_dob_label = Label(register_user_window,text='Date Of Birth').grid(row = 2 , column = 0 , padx = 20 , pady = 20)
-                    student_dob_entry = Entry(register_user_window)
-                    student_dob_entry.grid(row = 2 , column = 1 , padx = 20 , pady = 20)
-                    student_contact_label = Label(register_user_window,text='Mobile No : ').grid(row = 3 , column = 0 , padx = 20 , pady = 20)
-                    student_contact_entry = Entry(register_user_window)
-                    student_contact_entry.grid(row = 3 , column = 1 , padx = 20 , pady = 20)
-                    student_email_label = Label(register_user_window,text='E-mail : ').grid(row = 4 , column = 0 , padx = 20 , pady = 20)
-                    student_email_entry = Entry(register_user_window)
-                    student_email_entry.grid(row = 4 , column = 1 , padx = 20 , pady = 20)
+                    labelthisas(register_user_window,'Name : ',0,0)
+                    student_name_entry = entryfunction(register_user_window,0,1)
+                    labelthisas(register_user_window,'Roll no : ',1,0)
+                    student_roll_entry = entryfunction(register_user_window,1,1)
+                    labelthisas(register_user_window,'Date of Birth : ',2,0)
+                    student_dob_entry = entryfunction(register_user_window,2,1)
+                    labelthisas(register_user_window,'Contact no : ',3,0)
+                    student_contact_entry = entryfunction(register_user_window,3,1)
+                    labelthisas(register_user_window,'E-mail : ',4,0)
+                    student_email_entry = entryfunction(register_user_window,4,1)
 
                     options = ['Student','Teacher']
                     your_role = StringVar(register_user_window)
@@ -61,71 +67,51 @@ def login_window_function():
 
                     user_role = OptionMenu(register_user_window , your_role , *options )
                     user_role.grid(row = 6 , column = 1 , padx = 20 , pady = 20)
-                    user_role_label = Label(register_user_window , text='Role : ').grid(row=6 , column=0 , padx = 20 , pady = 20)
-                    student_password_label = Label(register_user_window,text='Password : ').grid(row = 5 , column = 0 , padx = 20 , pady = 20)
-                    student_password_entry = Entry(register_user_window)
-                    student_password_entry.grid(row = 5 , column = 1 , padx = 20 , pady = 20)
+                    labelthisas(register_user_window ,'Role : ',6,0)
+                    labelthisas(register_user_window,'Password',5,0)
+                    student_password_entry = entryfunction(register_user_window,5,1)
 
                     def register():
 
                         sql1 = (f"INSERT INTO student(sn,srn,sdob,scn,sei,ur,upass) VALUES(%s,%s,%s,%s,%s,%s,%s);")
-                        val1 = (student_name_entry.get(),student_roll_entry.get(),student_dob_entry.get(),student_contact_entry.get(),student_email_entry.get(),your_role.get(),student_password_entry.get())
+                        val1 = (student_name_entry.get(),student_roll_entry.get(),student_dob_entry.get(),student_contact_entry.get(),student_email_entry.get(),your_role.get(),student_password_entry.get() )
                         mycursor.execute(sql1 , val1)
                         print(mycursor)
                         conn.commit()
 
-                    register_btn = Button(register_user_window,text = 'Register' , command = register)
-                    register_btn.grid(row = 7 , column = 1 , padx = 20 , pady = 20)
+                    btn(register_user_window,'Register',register,7,1)
                     register_user_window.mainloop()
                 def admin_college_register_function():
-                    # root1 = Tk()
+
                     admin_college_register_window = Tk()
                     admin_college_register_window.geometry('800x600')
                     admin_college_register_window.title('Register new institute')
+                    labelthisas(admin_college_register_window,'Name of Institute',0,0)
+                    name_of_college_entry = entryfunction(admin_college_register_window,0,1)
+                    labelthisas(admin_college_register_window,'E-mail of Institute',1,0)
+                    email_of_college_entry = entryfunction(admin_college_register_window,1,1)
+                    labelthisas(admin_college_register_window,'Contact No. of Institute',2,0)
+                    contact_of_college_entry = entryfunction(admin_college_register_window,2,1)
+                    labelthisas(admin_college_register_window,'Branch Name of Institute',3,0)
+                    branch_of_college_entry = entryfunction(admin_college_register_window,3,1)
+                    labelthisas(admin_college_register_window,'Address of Institute',4,0)
+                    address_of_college_entry = entryfunction(admin_college_register_window,4,1) 
+                    labelthisas(admin_college_register_window,'Set password',5,0)
+                    password_of_college_entry = entryfunction(admin_college_register_window,5,1)
 
-                    name_of_college_label = Label(admin_college_register_window , text='Name Of The College').grid(row=0 , column=0)
-
-                    name_of_college_entry = Entry(admin_college_register_window )
-                    name_of_college_entry.grid(row=0 , column=1)
-
-                    email_of_college_label = Label(admin_college_register_window , text='E-mail Of The College').grid(row=1 , column=0)
-
-                    email_of_college_entry = Entry(admin_college_register_window )
-                    email_of_college_entry.grid(row=1 , column=1)
-
-                    contact_of_college_label = Label(admin_college_register_window , text='Contact Of The College').grid(row=2 , column=0)
-
-                    contact_of_college_entry = Entry(admin_college_register_window )
-                    contact_of_college_entry.grid(row=2 , column=1)
-
-                    branch_of_college_label = Label(admin_college_register_window , text='Branch Of The College').grid(row=3 , column=0)
-
-                    branch_of_college_entry = Entry(admin_college_register_window )
-                    branch_of_college_entry.grid(row=3 , column=1)
-
-                    address_of_college_label = Label(admin_college_register_window , text='Address Of The College').grid(row=4 , column=0)
-
-                    address_of_college_entry = Entry(admin_college_register_window )
-                    address_of_college_entry.grid(row=4 , column=1)
-                    
-                    password_of_college_label = Label(admin_college_register_window , text='Set Password').grid(row=5 , column=0)
-
-                    password_of_college_entry = Entry(admin_college_register_window )
-                    password_of_college_entry.grid(row=5 , column=1)
                     def register_institute_function():
                         sql2 = (f'INSERT INTO college_details(cn,ceid,ccn,cbr,cadd,cpass) VALUES(%s,%s,%s,%s,%s,%s);')
                         val2 = (name_of_college_entry.get() , email_of_college_entry.get() , contact_of_college_entry.get() , branch_of_college_entry.get() , address_of_college_entry.get() , password_of_college_entry.get())
 
                         mycursor.execute(sql2,val2)
                         conn.commit()
-                    register_institute_btn = Button(admin_college_register_window,text='Register Institute' , command=register_institute_function).grid(row=6 , column=1) 
+                    btn(admin_college_register_window,'Register Institute',register_institute_function,6,1) 
                     
                     admin_college_register_window.mainloop()
-                # admin_college_register_function()
                 
-                register_btn = Button(admin_window ,text="Add User", command=register_user_window_function).grid(row=3, column=1 , padx = 20 , pady = 20)
+                btn(admin_window,'Add User',register_user_window_function,3,1)
                 
-                institute_register_btn = Button(admin_window ,text="Add College", command=admin_college_register_function).grid(row=4, column=1 , padx = 20 , pady = 20)
+                btn(admin_window,'Add College',admin_college_register_function,4,1)
 
                 
                 admin_window.mainloop()
@@ -155,24 +141,18 @@ def login_window_function():
                 student_emailid = fetch_data[4]
                 student_password = fetch_data[5]
 
-                student_name1_label = Label(edit_window , text="Name: ").grid(row = 0 , column = 0 , padx = 20 , pady = 20)
-                student_name1_entry = Entry(edit_window)
-                student_name1_entry.grid(row = 0 , column = 1 , padx = 20 , pady = 20)
-                student_roll1_label = Label(edit_window,text='Roll no : ').grid(row = 1 , column = 0 , padx = 20 , pady = 20)
-                student_roll1_entry = Entry(edit_window)
-                student_roll1_entry.grid(row = 1 , column = 1 , padx = 20 , pady = 20)
-                student_dob1_label = Label(edit_window,text='Date Of Birth').grid(row = 2 , column = 0 , padx = 20 , pady = 20)
-                student_dob1_entry = Entry(edit_window)
-                student_dob1_entry.grid(row = 2 , column = 1 , padx = 20 , pady = 20)
-                student_contact1_label = Label(edit_window,text='Mobile No : ').grid(row = 3 , column = 0 , padx = 20 , pady = 20)
-                student_contact1_entry = Entry(edit_window)
-                student_contact1_entry.grid(row = 3 , column = 1 , padx = 20 , pady = 20)
-                student_email1_label = Label(edit_window,text='E-mail : ').grid(row = 4 , column = 0 , padx = 20 , pady = 20)
-                student_email1_entry = Entry(edit_window)
-                student_email1_entry.grid(row = 4 , column = 1 , padx = 20 , pady = 20)
-                student_password1_label = Label(edit_window,text='Password : ').grid(row = 5 , column = 0 , padx = 20 , pady = 20)
-                student_password1_entry = Entry(edit_window)
-                student_password1_entry.grid(row = 5 , column = 1 , padx = 20 , pady = 20)
+                labelthisas(edit_window,'Name' ,0,0)
+                student_name1_entry = entryfunction(edit_window,0,1)
+                labelthisas(edit_window,'Roll No, : ' ,1,0)
+                student_roll1_entry = entryfunction(edit_window,1,1)
+                labelthisas(edit_window,'Date of Birth : ' ,2,0)
+                student_dob1_entry = entryfunction(edit_window,2,1)
+                labelthisas(edit_window,'Contact No. : ' ,3,0)
+                student_contact1_entry =entryfunction(edit_window,3,1)
+                labelthisas(edit_window,'E-mail : ' ,4,0)
+                student_email1_entry = entryfunction(edit_window,4,1)
+                labelthisas(edit_window,'Password : ' ,5,0)
+                student_password1_entry = entryfunction(edit_window,5,1)
 
                 student_name1_entry.insert(0,student_name)
                 student_roll1_entry.insert(0,student_rollno)
@@ -191,9 +171,8 @@ def login_window_function():
                     conn.commit()
 
 
-
-                Update_btn = Button(edit_window , text='Update' , command=Update_entry).grid(row = 6 , column = 1 , padx = 20 , pady = 20)
-                delete_btn = Button(edit_window , text='Delete' , command=delete_entry).grid(row = 7 , column = 1 , padx = 20 , pady = 20)
+                btn(edit_window,'Update',Update_entry,6,1)
+                btn(edit_window,'Delete',delete_entry,7,1)
 
                 edit_window.mainloop()
             def read_data():
@@ -212,12 +191,14 @@ def login_window_function():
                 root4.mainloop()
 
                 after_login_window.mainloop()
-            read_btn = Button(after_login_window , text='Dasplay Data' , command=read_data).grid(row=0 , column=1)
 
-            update_btn = Button(after_login_window , text='Edit' ,command=update_window_function).grid(row= 1 ,column=1) 
+            btn(after_login_window,'Display Data',read_data,0,1)
+ 
+            btn(after_login_window,'Edit',update_window_function,1,1)
 
 
     login_btn = Button(login_window ,text="Login", command=after_login).grid(row=2, column=1 , padx = 20 , pady = 20)
+    btn(login_window,'Login',after_login,2,1)  
 
 
     login_window.mainloop()
