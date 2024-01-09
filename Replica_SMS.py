76d6bf94a1a574c2554298378d4f2b17ee707b9b
umang-orpadia <orpadiau@gmail.com>
2024-01-09 21:48:28 +0530

    Improved Code Readability

--- Replica_SMS.py
+++ Replica_SMS.py
@@ -48,6 +48,124 @@ def update_function(name ,rollno , dob ,cn , ei ,upass , ur ,basedon ):
     print(mycursor)
     conn.commit()
 
+def register_user_window_function():
+
+    register_user_window = Tk()
+    register_user_window.geometry('1500x900')
+    register_user_window.title('This is registration Window')
+    register_user_window.configure(background='purple')
+
+
+    labelthisas(register_user_window,'Name : ',0,0)
+    student_name_entry = entryfunction(register_user_window,0,1)
+    labelthisas(register_user_window,'Roll no : ',1,0)
+    student_roll_entry = entryfunction(register_user_window,1,1)
+    labelthisas(register_user_window,'Date of Birth : ',2,0)
+    student_dob_entry = entryfunction(register_user_window,2,1)
+    labelthisas(register_user_window,'Contact no : ',3,0)
+    student_contact_entry = entryfunction(register_user_window,3,1)
+    labelthisas(register_user_window,'E-mail : ',4,0)
+    student_email_entry = entryfunction(register_user_window,4,1)
+
+    options = ['Student','Teacher']
+    your_role = StringVar(register_user_window)
+    your_role.set('Student')
+
+    user_role = OptionMenu(register_user_window , your_role , *options )
+    user_role.grid(row = 6 , column = 1 , padx = 20 , pady = 20)
+    labelthisas(register_user_window ,'Role : ',6,0)
+    labelthisas(register_user_window,'Password',5,0)
+    student_password_entry = entryfunction(register_user_window,5,1)
+
+    def insertit():
+        insertfunction(student_name_entry.get(),student_roll_entry.get(),student_dob_entry.get(),student_contact_entry.get(),student_email_entry.get(),your_role.get(),student_password_entry.get())
+
+    btn(register_user_window,'Register',insertit,7,1)
+    register_user_window.mainloop()
+    
+def admin_window_function():
+    admin_window = Tk()
+    admin_window.title('You are Logedin as admin')
+    admin_window.geometry('800x700')
+
+    btn(admin_window,'Add User',register_user_window_function,3,1)
+    
+    # btn(admin_window,'Add College',admin_college_register_function,4,1)
+    # btn(admin_window,'Display Data',read_data,5,1)
+
+    
+    admin_window.mainloop()
+    
+
+def update_window_function():
+
+    edit_window = Tk()
+    edit_window.geometry("800x600")
+    edit_window.title("This is Update Page")
+    edit_window.configure(background='pink')
+
+
+    sql2 = ( f' SELECT * FROM student WHERE sei = %s AND upass = %s ')
+    val2 = (enteredusername,enteredpassword)
+    mycursor.execute(sql2 , val2)
+
+    fetch_data = mycursor.fetchone()
+    print(fetch_data)
+    student_name = fetch_data[0]
+    student_rollno = fetch_data[1]
+    student_dob = fetch_data[2]
+    student_contactno = fetch_data[3]
+    student_emailid = fetch_data[4]
+    student_password = fetch_data[5]
+    user_role = fetch_data[6]
+
+    labelthisas(edit_window,'Name' ,0,0)
+    student_name1_entry = entryfunction(edit_window,0,1)
+    labelthisas(edit_window,'Roll No, : ' ,1,0)
+    student_roll1_entry = entryfunction(edit_window,1,1)
+    labelthisas(edit_window,'Date of Birth : ' ,2,0)
+    student_dob1_entry = entryfunction(edit_window,2,1)
+    labelthisas(edit_window,'Contact No. : ' ,3,0)
+    student_contact1_entry =entryfunction(edit_window,3,1)
+    labelthisas(edit_window,'E-mail : ' ,4,0)
+    student_email1_entry = entryfunction(edit_window,4,1)
+    labelthisas(edit_window,'Password : ' ,5,0)
+    student_password1_entry = entryfunction(edit_window,5,1)
+    labelthisas(edit_window,'Role : ' ,6,0)
+    # student_role_entry = entryfunction(edit_window,6,1)
+
+    student_name1_entry.insert(0,student_name)
+    student_roll1_entry.insert(0,student_rollno)
+    student_dob1_entry.insert(0,student_dob)
+    student_contact1_entry.insert(0,student_contactno)
+    student_email1_entry.insert(0,student_emailid)
+    student_password1_entry.insert(0,student_password)
+    # student_role_entry.insert(0,user_role)
+
+    options = ['Student','Teacher']
+    your_role = StringVar(edit_window)
+    your_role.set(user_role)
+
+    user_role1 = OptionMenu(edit_window , your_role , *options )
+    user_role1.grid(row = 6 , column = 1 , padx = 20 , pady = 20)
+
+    def try_update():
+        update_function(student_name1_entry.get(),student_roll1_entry.get(),student_dob1_entry.get(),student_contact1_entry.get(),student_email1_entry.get(),student_password1_entry.get(),your_role.get(),student_rollno)
+
+    def delete_entry():
+        mycursor.execute(f"DELETE FROM student WHERE sei = '{student_email1_entry.get()}'")
+        conn.commit()
+
+
+    btn(edit_window,'Update',try_update,7,1)
+    btn(edit_window,'Delete',delete_entry,8,1)
+
+    edit_window.mainloop()
+
+
+    
+
+
 def login_window_function():
 
 
@@ -72,82 +190,6 @@ def login_window_function():
         print(enteredusername)
         print(enteredpassword)
         if enteredusername == 'admin' and enteredpassword == 'admin':
-            def admin_window_function():
-                admin_window = Tk()
-                admin_window.title('You are Logedin as admin')
-                admin_window.geometry('800x700')
-
-
-                def register_user_window_function():
-
-                    register_user_window = Tk()
-                    register_user_window.geometry('1500x900')
-                    register_user_window.title('This is registration Window')
-                    register_user_window.configure(background='purple')
-
-
-                    labelthisas(register_user_window,'Name : ',0,0)
-                    student_name_entry = entryfunction(register_user_window,0,1)
-                    labelthisas(register_user_window,'Roll no : ',1,0)
-                    student_roll_entry = entryfunction(register_user_window,1,1)
-                    labelthisas(register_user_window,'Date of Birth : ',2,0)
-                    student_dob_entry = entryfunction(register_user_window,2,1)
-                    labelthisas(register_user_window,'Contact no : ',3,0)
-                    student_contact_entry = entryfunction(register_user_window,3,1)
-                    labelthisas(register_user_window,'E-mail : ',4,0)
-                    student_email_entry = entryfunction(register_user_window,4,1)
-
-                    options = ['Student','Teacher']
-                    your_role = StringVar(register_user_window)
-                    your_role.set('Student')
-
-                    user_role = OptionMenu(register_user_window , your_role , *options )
-                    user_role.grid(row = 6 , column = 1 , padx = 20 , pady = 20)
-                    labelthisas(register_user_window ,'Role : ',6,0)
-                    labelthisas(register_user_window,'Password',5,0)
-                    student_password_entry = entryfunction(register_user_window,5,1)
-
-                    def insertit():
-                        insertfunction(student_name_entry.get(),student_roll_entry.get(),student_dob_entry.get(),student_contact_entry.get(),student_email_entry.get(),your_role.get(),student_password_entry.get())
-
-                    btn(register_user_window,'Register',insertit,7,1)
-                    register_user_window.mainloop()
-                def admin_college_register_function():
-
-                    admin_college_register_window = Tk()
-                    admin_college_register_window.geometry('800x600')
-                    admin_college_register_window.title('Register new institute')
-                    labelthisas(admin_college_register_window,'Name of Institute',0,0)
-                    name_of_college_entry = entryfunction(admin_college_register_window,0,1)
-                    labelthisas(admin_college_register_window,'E-mail of Institute',1,0)
-                    email_of_college_entry = entryfunction(admin_college_register_window,1,1)
-                    labelthisas(admin_college_register_window,'Contact No. of Institute',2,0)
-                    contact_of_college_entry = entryfunction(admin_college_register_window,2,1)
-                    labelthisas(admin_college_register_window,'Branch Name of Institute',3,0)
-                    branch_of_college_entry = entryfunction(admin_college_register_window,3,1)
-                    labelthisas(admin_college_register_window,'Address of Institute',4,0)
-                    address_of_college_entry = entryfunction(admin_college_register_window,4,1) 
-                    labelthisas(admin_college_register_window,'Set password',5,0)
-                    password_of_college_entry = entryfunction(admin_college_register_window,5,1)
-
-                    def register_institute_function():
-                        sql2 = (f'INSERT INTO college_details(cn,ceid,ccn,cbr,cadd,cpass) VALUES(%s,%s,%s,%s,%s,%s);')
-                        val2 = (name_of_college_entry.get() , email_of_college_entry.get() , contact_of_college_entry.get() , branch_of_college_entry.get() , address_of_college_entry.get() , password_of_college_entry.get())
-
-                        mycursor.execute(sql2,val2)
-                        conn.commit()
-                    btn(admin_college_register_window,'Register Institute',register_institute_function,6,1) 
-                    
-                    admin_college_register_window.mainloop()
-
-                
-                btn(admin_window,'Add User',register_user_window_function,3,1)
-                
-                btn(admin_window,'Add College',admin_college_register_function,4,1)
-                # btn(admin_window,'Display Data',read_data,5,1)
-
-                
-                admin_window.mainloop()
             admin_window_function()
         else :
             sql2 = ( f' SELECT * FROM student WHERE sei = %s AND upass = %s ')
@@ -159,77 +201,12 @@ def login_window_function():
                 after_login_window =Tk()
                 after_login_window.geometry('500x300')
                 after_login_window.config(bg='green')
-                def update_window_function():
-
-                    edit_window = Tk()
-                    edit_window.geometry("800x600")
-                    edit_window.title("This is Update Page")
-                    edit_window.configure(background='pink')
-
-
-                    sql2 = ( f' SELECT * FROM student WHERE sei = %s AND upass = %s ')
-                    val2 = (enteredusername,enteredpassword)
-                    mycursor.execute(sql2 , val2)
-
-                    fetch_data = mycursor.fetchone()
-                    print(fetch_data)
-                    student_name = fetch_data[0]
-                    student_rollno = fetch_data[1]
-                    student_dob = fetch_data[2]
-                    student_contactno = fetch_data[3]
-                    student_emailid = fetch_data[4]
-                    student_password = fetch_data[5]
-                    user_role = fetch_data[6]
-
-                    labelthisas(edit_window,'Name' ,0,0)
-                    student_name1_entry = entryfunction(edit_window,0,1)
-                    labelthisas(edit_window,'Roll No, : ' ,1,0)
-                    student_roll1_entry = entryfunction(edit_window,1,1)
-                    labelthisas(edit_window,'Date of Birth : ' ,2,0)
-                    student_dob1_entry = entryfunction(edit_window,2,1)
-                    labelthisas(edit_window,'Contact No. : ' ,3,0)
-                    student_contact1_entry =entryfunction(edit_window,3,1)
-                    labelthisas(edit_window,'E-mail : ' ,4,0)
-                    student_email1_entry = entryfunction(edit_window,4,1)
-                    labelthisas(edit_window,'Password : ' ,5,0)
-                    student_password1_entry = entryfunction(edit_window,5,1)
-                    labelthisas(edit_window,'Role : ' ,6,0)
-                    # student_role_entry = entryfunction(edit_window,6,1)
-
-                    student_name1_entry.insert(0,student_name)
-                    student_roll1_entry.insert(0,student_rollno)
-                    student_dob1_entry.insert(0,student_dob)
-                    student_contact1_entry.insert(0,student_contactno)
-                    student_email1_entry.insert(0,student_emailid)
-                    student_password1_entry.insert(0,student_password)
-                    # student_role_entry.insert(0,user_role)
-
-                    options = ['Student','Teacher']
-                    your_role = StringVar(edit_window)
-                    your_role.set(user_role)
-
-                    user_role1 = OptionMenu(edit_window , your_role , *options )
-                    user_role1.grid(row = 6 , column = 1 , padx = 20 , pady = 20)
-
-                    def try_update():
-                        update_function(student_name1_entry.get(),student_roll1_entry.get(),student_dob1_entry.get(),student_contact1_entry.get(),student_email1_entry.get(),student_password1_entry.get(),your_role.get(),student_rollno)
-
-                    def delete_entry():
-                        mycursor.execute(f"DELETE FROM student WHERE sei = '{student_email1_entry.get()}'")
-                        conn.commit()
-
-
-                    btn(edit_window,'Update',try_update,7,1)
-                    btn(edit_window,'Delete',delete_entry,8,1)
-
-                    edit_window.mainloop()
                 
-
-                    after_login_window.mainloop()
-
                 btn(after_login_window,'Display Data',read_data,0,1)
     
                 btn(after_login_window,'Edit',update_window_function,1,1)
+
+                after_login_window.mainloop()
             else :
                 print('Something went wrong')
 
